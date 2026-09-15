4c250c2372c8710ad6cc84bd44764cb85be0b330
Ryan Nguyen <ryanhuunguyen@icloud.com>
2026-09-15 14:36:01 -0400

    Buttons and Logos

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("magicBtn").onclick = () => {
+    alert("Magic button clicked!");
+};
+
+document.getElementById("mmaBtn").onclick = () => {
+    alert("Merchant Marine Academy clicked!");
+};
+
+document.getElementById("navyBtn").onclick = () => {
+    alert("Navy Application clicked!");
+};
+
+document.getElementById("armyBtn").onclick = () => {
+    alert("Army Application clicked!");
+};
+
+document.getElementById("airforceBtn").onclick = () => {
+    alert("Air Force Application clicked!");
+};
+
+document.getElementById("coastguardBtn").onclick = () => {
+    alert("Coast Guard Application clicked!");
+};
